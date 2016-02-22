ed6fe1f2847ba23c8390d24645d5015ad7e36305
Filippo Locoro <filippo.locoro@gmail.com>
2016-02-22 17:21:54 +0100

    init

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var elixir = require('laravel-elixir');
+//https://www.npmjs.com/package/laravel-elixir-livereload
+require('laravel-elixir-livereload');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+//elixir.config.sourcemaps = false;
+
+elixir(function(mix) {
+   mix.sass('bootstrap.scss')
+   .sass('app.scss')
+   .sass('variables.scss');
+
+   mix.copy('resources/bower_box/jquery/dist/jquery.js' , 'public/js/vendor')
+   .copy('resources/bower_box/velocity/velocity.js' , 'public/js/vendor')
+   .copy('resources/bower_box/velocity/velocity.ui.js' , 'public/js/vendor')
+   .copy('resources/bower_box/tether/dist/js/tether.js' , 'public/js/vendor')
+   .copy('resources/bower_box/bootstrap/dist/js/bootstrap.js' , 'public/js/vendor');
+
+   mix.styles([ 'bootstrap.css' , 'variables.css' , 'app.css' ] , 'public/css/all.css' , 'public/css');
+
+   mix.version('public/css/all.css');
+
+   mix.scripts([ 'jquery.js' , 'velocity.js' , 'velocity.ui.js' , 'tether.js' , 'bootstrap.js'] , 'public/js/all.js' , 'public/js/vendor');
+
+   mix.livereload();
+});
+
+// gulp --production
---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <link href="{{ elixir('css/all.css')}}" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+
+    <!-- Livereload -->
+    @if ( Config::get('app.debug') )
+    <script type="text/javascript">
+        document.write('<script src="//localhost:35729/livereload.js?snipver=1" type="text/javascript"><\/script>')
+    </script> 
+    @endif
+    <!-- Livereload -->
+
+    <!--[if lt IE 9]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+
+
+    <script src="js/all.js"></script>
+
+</body>
+</html>
